c9735781923a56190ca41c4428e16de1029f419a
dchf <dchf.jp@gmail.com>
2022-10-08 19:37:57 +0900

    feat:パスから親ディレクトリを取得

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
-	fileName := "default.md"
+	fileName := "drafts/default.md"
+	//fileName := "drafts/"
 	fmt.Println(fileName)
-	dir, err := filepath.Abs(fileName)
+	path, err := filepath.Abs(fileName)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println(dir)
+		fmt.Println(filepath.Dir(path))
 	}
 }

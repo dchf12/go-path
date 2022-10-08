89ebcf0965d996de7451c7df166713676afed07b
dchf <dchf.jp@gmail.com>
2022-10-08 19:42:39 +0900

    feat:指定された絶対パスの最後のファイル/フォルダーを取得

--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 )
 
 func main() {
-	fileName := "drafts/default.md"
-	//fileName := "drafts/"
-	fmt.Println(fileName)
-	path, err := filepath.Abs(fileName)
+	fileName := "default.md"
+	dir, err := filepath.Abs(fileName)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println(filepath.Dir(path))
+		fmt.Println(err)
+		fmt.Println(filepath.Base(dir))
 	}
 }
